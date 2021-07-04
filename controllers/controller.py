7b22afddf8a4dd3a5c942e7aebdca9e793429525
jakubnecessary <cabbage1991@gmail.com>
2021-07-04 17:56:11 +0100

    stil working on condtionial logic in html and trying to get route to actually be able to post

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,4 +7,11 @@ from models.game import *
 def index():
     return render_template('index.html', title="RPS", players=players)
 
+@app.route('/game', methods=['POST'])
+def add_game():
+    if player_one.request.form["choice"] == "Rock" 
+    and player_two.request.form["choice"] == "Rock"
+    return "Tie"
+
+
 
