9918dcb48dd589377d6cfea0b45f904cba380b7a
jakubnecessary <cabbage1991@gmail.com>
2021-07-04 18:42:26 +0100

    got game logic if statement in game.py

--- models/game.py
+++ models/game.py
@@ -11,8 +11,24 @@ def add_new_player(player):
     players.append(player)
 
 def game_logic():
+    # player 1 rock
     if player_1.choice == "Rock" and player_2.choice == "Rock":
         return "Tie"
     elif player_1.choice == "Rock" and player_2.choice == "Scissors":
         return "Rock beats Scissors"
-    elif player_1.choice
+    elif player_1.choice == "Rock" and player_2.choice == "Paper":
+        return "Paper Beats Rock"
+    # player 1 paper
+    elif player_1.choice == "Paper" and player_2.choice == "Rock":
+        return "Paper Beats Rock"
+    elif player_1.choice == "Paper" and player_2.choice == "Scissors":
+        return "Scissors Beats Paper"
+    elif player_1.choice == "Paper" and player_2.choice == "Paper":
+        return "Tie"
+    # player 1 scissors
+    elif player_1.choice == "Scissors" and player_2.choice == "Rock":
+        return "Rock Beats Scissors"
+    elif player_1.choice == "Scissors" and player_2.choice == "Scissors":
+        return "Tie"
+    elif player_1.choice == "Scissors" and player_2.choice == "Paper":
+        return "Scissors Beats Paper"
